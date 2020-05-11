120d07464609a488e5f5b2d069e0977ab8ef9798
Benjamin <58241833+Papr3ka@users.noreply.github.com>
2020-05-10 21:28:36 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -6,7 +6,8 @@ import time
 import math
 from decimal import *
 from decimal import Decimal as dec
-getcontext().prec = 32768
+precision = 768
+getcontext().prec = precision
 def clear():
     if platform.system() == 'Windows':
         os.system('cls')
@@ -22,7 +23,132 @@ def factorial(x):
     return x
 def sqrt(x):
 	ans = dec(x)**dec((dec(1))/dec(2))
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
 	return ans
+def abs(x):
+	if x < 0:
+		ans = x * -1
+	else:
+		ans = x
+	return ans
+def ceil(x):
+	ans = int(int(x) + 1)
+	return ans
+def floor(x):
+	ans = int(x)
+	return ans
+def constant(precision):
+	print("Loading Constants...")
+	global pi
+	global e
+	pi = 0
+	e = 0
+	for x in range(0,precision): 
+		pi += dec((dec(1) / dec(16) ** dec(x)) * (dec(4) / (dec(8) * x + dec(1)) - dec(2) / (dec(8) * x + dec(4)) - dec(1) / (dec(8) * x + dec(5)) - dec(1) / (dec(8) * x + dec(6))))
+	pi = dec(int(pi * dec(10 ** precision)) / dec(10 ** precision))
+	for x in range(0,precision + 64):
+		e += dec(dec(1) / dec(factorial(int(x))))
+	e = dec(int(e * dec(10 ** precision)) / dec(10 ** precision))
+def bernoulli(n):
+    A = [0] * (n+1)
+    for m in range(n+1):
+        A[m] = dec(1)/(m+dec(1))
+        for j in range(m, 0, -1):
+          A[j-1] = j*(A[j-1] - A[j])
+    return A[0]
+# Taylor series Expansion of Trigonomic functions
+def sin(x):
+	ans = 0
+	for n in range(0,int(precision)+int(abs(x))):
+	 	ans += dec((dec(-1)**n)*x**(dec(2)*n+dec(1)))/dec(factorial(2*n+1))
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def cos(x):
+	ans = 0
+	for n in range(0,int(precision)+int(abs(x))):
+	 	ans += dec((dec(-1)**n)*x**(dec(2)*n))/dec(factorial(2*n))
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def tan(x):
+	ans = sin(x)/cos(x)
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def arcsin(x):
+	ans = 0
+	for n in range(0,int(precision)+int(x)):
+		ans += dec(factorial(2*n))*x**dec(dec(2)*n+dec(1))/dec(4)**n*dec(factorial(n))**dec(2)*dec(dec(2)*n+dec(1))
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def arccos(x):
+	pass
+def arctan(x):
+	pass
+def sinh(x):
+	ans = (e**x-e**(-x))/dec(2)
+	return ans
+def cosh(x):
+	ans = (e**x+e**(-x))/dec(2)
+	return ans
+def tanh(x):
+	ans = (e**x-e**(-x))/((e**x)+e**(-x))
+	return ans
+def coth(x):
+	ans = (e**x+e**-x)/(e**x-e**-x)
+	return ans
+def sech(x):
+	ans = dec(2)/(e**x+e**-x)
+	return ans
+def csch(x):
+	ans = dec(2)/(e**x-e**-x)
+	return ans
+def arcsinh(x):
+	pass
+def arccosh(x):
+	pass
+def arctanh(x):
+	pass
+def sec(x):
+	ans = dec(1)/cos(x)
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def csc(x):
+	ans = dec(1)/sin(x)
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def cot(x):
+	ans = dec(1)/tan(x)
+	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+	return ans
+def arcsec(x):
+	pass
+def arccsc(x):
+	pass
+def arccot(x):
+	pass
+def arcsech(x):
+	pass
+def arccsch(x):
+	pass
+def arccoth(x):
+	pass
+# Incomplete WIP
+# Error Function
+def erf(z):
+#	ans = 0
+#	for n in range(0,precision+int(z)+1000):
+#		ans += dec((dec(-1)**n)*z**dec(2)*n+dec(1))/dec(factorial(n))*(dec(2)*n+dec(1))
+#	ans = ans*(dec(2)/sqrt(pi))
+#	ans = dec(int(ans * dec(10 ** precision)) / dec(10 ** precision))
+#	return ans
+	pass
+def exp(x):
+	ans = e**x
+	return ans
+# Gamma Function
+pass
+# Finite Integral
+def fnint(var,eq,min,max):
+	pass
 def Eq_fix(run_string):
 	for x in range(1, len(run_string)):
 		run_string = run_string.replace("^","**")
@@ -48,6 +174,12 @@ def Eq_fix(run_string):
 		run_string = run_string.replace(")8", ")*8")
 		run_string = run_string.replace(")9", ")*9")
 		run_string = run_string.replace("Factorial", "factorial")
+		run_string = run_string.replace("Pi", "pi")
+		run_string = run_string.replace("PI", "pi")
+		run_string = run_string.replace("E", "e")
+		run_string = run_string.replace("Euler", "e")
+		run_string = run_string.replace("euler", "e")
+# Section for future string corrections
 	return run_string
 def getcalc():
 	clear()
@@ -57,7 +189,7 @@ def getcalc():
 	clear()
 	print("Calculating...")
 	try:
-		final_ans = eval(calc_str)
+		final_ans = dec(eval(calc_str))
 		clear()
 	except FloatingPointError:
 		print("ERROR:FloatingPointError")
@@ -80,5 +212,7 @@ def getcalc():
 		print("\n")
 		print("Compute time:",'%.4f'%(tend - tstart),"Seconds")
 		print("I/O time:",'%.4f'%(tend_copy - tstart_copy),"Seconds")
+		print("Total time:",'%.4f'%(tend - tstart + tend_copy - tstart_copy),"Seconds")
+constant(precision)
 getcalc()
 wait()
